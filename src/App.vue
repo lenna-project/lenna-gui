839c00d6da9a56a8f2a428d9039c2997110922e4
Christian M <chriamue@gmail.com>
2021-07-30 11:28:31 +0200

    :sparkles: added about page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <About />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import About from './views/About.vue'
+import Home from './views/Home.vue'
 
 export default {
-  name: 'App',
+  name: 'Lenna App',
   components: {
-    HelloWorld
+    About,
+    Home
   }
 }
 </script>
---
+++ src/views/Home.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="main">
+      <h1>Convert images online without upload of your data</h1>
+      <h2 v-if="just">{{ just }}</h2>
+      <div class="top_main">
+        <div id="process">
+          <button class="v-step-5" v-on:click="processImages">
+            process images
+          </button>
+        </div>
+      </div>
+      <div class="bottom_main">
+      </div>
+      <div id="line">
+        <hr />
+        <div class="plus radius" v-on:click="onMorePlugins()"></div>
+      </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+  },
+});
+</script>
+<style scoped lang="scss">
+@import "@/styles/_variables.scss";
+@import url("https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap");
+
+.main {
+  background-color: $body_background;
+  padding: 150px;
+  padding-top: 120px;
+  text-align: center;
+}
+.main h1 {
+  text-transform: uppercase;
+  font-family: "Amatic SC", cursive;
+  font-size: 32pt;
+}
+.main hr {
+  margin-top: 10px;
+  border: none;
+  border-top: 2px dotted blue;
+  color: #fff;
+  background-color: #fff;
+  height: 2px;
+  width: 100%;
+}
+.top_main {
+  display: flex;
+  justify-content: space-between;
+}
+.bottom_main {
+  padding-top: 20px;
+  padding-bottom: 0;
+}
+#process button {
+  margin-top: 40%;
+  width: 350px;
+  align-self: center;
+  font-size: 20pt;
+  text-transform: uppercase;
+  padding: 12px;
+  border-radius: 30px;
+  border: none;
+  box-shadow: 5px 5px 5px $shadow;
+}
+#process button:hover {
+  transform: scale(1.05);
+}
+#line {
+  margin-top: 50px;
+  position: relative;
+}
+
+.plus {
+  position: absolute;
+  top: -25px;
+  left: calc(50% - 20px);
+
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+
+  background: linear-gradient(#fff, #fff), linear-gradient(#fff, #fff), blue;
+  background-position: center;
+  background-size: 50% 2px, 2px 50%; /*thickness = 2px, length = 50% (25px)*/
+  background-repeat: no-repeat;
+}
+.radius {
+  border-radius: 50%;
+}
+.plus:hover {
+  transform: scale(1.1);
+}
+
+@media screen and (max-width: 800px) {
+  .main {
+    margin-top: 120px;
+    padding: 0;
+  }
+  #process button {
+    width: 150px;
+  }
+}
+</style>
