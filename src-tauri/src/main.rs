f3383a73dbc43fca8b0e2d8075b8e818677c074d
Christian M <chriamue@gmail.com>
2021-08-01 13:11:17 +0200

    :sparkles: use tauri state

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,9 @@
 )]
 
 use lenna_cli::plugins;
-use lenna_core::Config;
+use lenna_core::{Config, Pool};
+
+struct State(Pool);
 
 #[tauri::command]
 async fn get_config() -> Result<Config, String> {
@@ -14,17 +16,18 @@ async fn get_config() -> Result<Config, String> {
 }
 
 #[tauri::command]
-async fn get_plugin_ids() -> Result<Vec<String>, String> {
-  let mut plugins = plugins::Plugins::new();
-  let plugins_path = match std::env::var("LENNA_PLUGINS") {
-    Ok(val) => std::path::PathBuf::from(val),
-    _ => std::path::PathBuf::from("plugins/"),
-  };
-
-  plugins.load_plugins(&plugins_path);
-
-  let plugin_ids: Vec<String> = plugins.pool.ids();
+async fn get_plugin_ids(state: tauri::State<'_, State>) -> Result<Vec<String>, String> {
+  let plugin_ids: Vec<String> = state.0.ids();
   Ok(plugin_ids)
+}
+
+#[tauri::command]
+async fn get_plugin(state: tauri::State<'_, State>, id: String) -> Result<String, String> {
+  let plugin = state.0.get(&id);
+  match plugin {
+    Some(plugin) => Ok(plugin.description()),
+    _ => Err("No such plugin".into()),
+  }
 }
 
 #[tauri::command]
@@ -33,8 +36,26 @@ fn my_custom_command() -> String {
 }
 
 fn main() {
+  let mut plugins = plugins::Plugins::new();
+  let plugins_path = match std::env::var("LENNA_PLUGINS") {
+    Ok(val) => std::path::PathBuf::from(val),
+    _ => std::path::PathBuf::from("plugins/"),
+  };
+
+  plugins.load_plugins(&plugins_path);
+
+  let pool: Pool = Pool::default();
+
+  let state = State { 0: pool };
+
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![my_custom_command, get_config, get_plugin_ids])
+    .manage(state)
+    .invoke_handler(tauri::generate_handler![
+      my_custom_command,
+      get_config,
+      get_plugin_ids,
+      get_plugin
+    ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
